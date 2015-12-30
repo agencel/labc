d91b34e22fe02b9299220bb5374cc1d343d54cab
mattiasquat <mattias.kaminski@gmail.com>
2015-12-30 03:11:33 +0100

    Update 5.c

--- rekurencja/5.c
+++ rekurencja/5.c
@@ -1,23 +1,36 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
+#include <math.h>
 
-int zamiana(int n)
+void zamiana(int liczba)
 {
-  int tab[255];
-  int i=0,j;
-  int n,a;
-  scanf("%d",&n);
-  for(n;n>=1;n=n/2){
-    i+=1;
-    tab[i]=n%2;
-  }
-  for(j=i;j>0;j--)
-  {
-    printf("%d",tab[j]);
-  }
-  printf("\n");
+	if(liczba>0)
+	{
+		zamiana(liczba/2);
+		printf("%d",liczba%2);
+	}
+	if(liczba==0)
+	{
+		printf("\n");
+	}
+	
 }
+void ciag(int n)
+{
+	if(n>0)
+	{
+		zamiana(n-1);
+		ciag(n-1);
+	}else if(n==0)
+	{
+		printf("0");
+	}
+}
+
 int main()
 {
-
+	int n;
+	printf("Podaj n-ty wyraz ciagu binarnego: ");
+	scanf("%d",&n);
+	ciag(pow(2,n));
 }
